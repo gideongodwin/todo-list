ccb80596a80c7a10c03b42c7f9e8a18653e67c4d
Gideon Godwin <gideongodwin56@gmail.com>
2022-11-08 21:28:42 +0100

    added some changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {
+  theme: { 
+    screens:{
+    'xs': {'max': '480px'},
+  },
     extend: {
       fontFamily:{
         "bauman": ['Baumans', 'cursive'],
-        "signika":['Signika Negative', 'sans-serif']
+        "signika":['Signika Negative', 'sans-serif'],
+        "kanit":['Kanit', 'sans-serif'],
       },
     },
   },
